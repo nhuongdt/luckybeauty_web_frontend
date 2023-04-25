9fd9347b26b209b6be02fab75c6d3d58cf33ca84
ducmanhssoft <ducmanh.ssoft@gmail.com>
2023-04-25 09:44:57 +0700

    merge form main

--- src/pages/tenant/create-or-edit-tenant.tsx
+++ src/pages/tenant/create-or-edit-tenant.tsx
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 import {
   Checkbox,
   Col,
@@ -11,39 +10,16 @@ import {
 
 import rules from "./createOrUpdateTenant.validation";
 import React, { useState } from "react";
-=======
-import { Checkbox, Form, Input, Modal, Typography } from 'antd';
-import React, { useState } from 'react';
-
-
-
-
-
-
-
-
-
-
->>>>>>> .theirs
 const { Text, Link } = Typography;
 
 export interface ICreateOrEditTenantProps {
-<<<<<<< .mine
   visible: boolean;
   onCancel: () => void;
   modalType: string;
   tenantId: number;
   onOk: () => void;
-=======
-    visible: boolean;
-    onCancel: () => void;
-    modalType: string;
-    onOk: () => void;
-
->>>>>>> .theirs
   formRef: React.RefObject<FormInstance>;
 }
-<<<<<<< .mine
 const CreateOrEditTenant = ({
   visible,
   onCancel,
@@ -116,80 +92,6 @@ const CreateOrEditTenant = ({
     </Modal>
   );
 };
-=======
-const CreateOrEditTenant = ({ visible, onCancel, modalType, onOk }: ICreateOrEditTenantProps) => {
-    const [isHostDatabase, setIsHostDataBase] = useState(false);
-    const [isPasswordStand, setIsPasswordStand] = useState(true);
-    return (
-        <Modal
-            visible={visible}
-            onCancel={onCancel}
-            title={modalType}
-            okText="Ok"
-            cancelText="Đóng"
-            width={648}
-            onOk={onOk}>
-            <Form layout="vertical" className="mt-5">
-                <Form.Item>
-                    <Text strong>Tenant Name</Text>
-                    <Input size="large" placeholder="Nhập tên tenant" required />
-                </Form.Item>
-                <Form.Item>
-                    <Text strong>Display Name</Text>
-                    <Input size="large" placeholder="input placeholder" required />
-                </Form.Item>
-                <Form.Item>
-                    <Text strong>Email quản trị</Text>
-                    <Input size="large" placeholder="Nhập email quản trị" required />
-                </Form.Item>
-                <Form.Item>
-                    <Checkbox
-                        value={isHostDatabase}
-                        checked={isHostDatabase}
-                        onChange={() => {
-                            setIsHostDataBase(!isHostDatabase);
-                        }}>
-                        Dùng chung cơ sở dữ liệu với Host
-                    </Checkbox>
-                </Form.Item>
-                <Form.Item hidden={isHostDatabase}>
-                    <Text strong>Connection Strings</Text>
-                    <Input size="large" placeholder="input placeholder" />
-                </Form.Item>
-                <Form.Item>
-                    <Checkbox
-                        value={isPasswordStand}
-                        checked={isPasswordStand}
-                        onChange={() => {
-                            setIsPasswordStand(!isPasswordStand);
-                        }}>
-                        Mật khẩu mặc định
-                    </Checkbox>
-                </Form.Item>
-                <Form.Item hidden={isPasswordStand}>
-                    <Text strong>Mật khẩu quản trị</Text>
-                    <Input size="large" placeholder="input placeholder" />
-                </Form.Item>
-                <Form.Item hidden={isPasswordStand}>
-                    <Text strong>Nhập lại mật khẩu quản tri</Text>
-                    <Input size="large" placeholder="input placeholder" />
-                </Form.Item>
-            </Form>
-        </Modal>
-    );
-};
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> .theirs
 
 export default CreateOrEditTenant;
 
