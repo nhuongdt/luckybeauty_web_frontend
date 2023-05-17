264648ca18630e891fd57dcf11742055ca9d5e38
Tài Đinh <dinhtuantaiok@gmail.com>
2023-05-17 03:30:44 -0700

    convert mui

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Input, Checkbox, Avatar, Row, Col } from 'antd';
+import { Grid, Input, Checkbox, Avatar, Box, TextField, FormControlLabel } from '@mui/material';
 import './login.css';
 import LoginModel from '../../models/Login/loginModel';
 import LoginService from '../../services/login/loginService';
@@ -8,54 +8,6 @@ import { useNavigate } from 'react-router-dom';
 import logo from '../../images/Lucky_beauty.jpg';
 import { Link } from 'react-router-dom';
 const LoginScreen: React.FC = () => {
-    const loginModel = new LoginModel();
-    const [remember, setRemember] = useState(false);
-    const [form] = Form.useForm();
-    const navigate = useNavigate();
-    const handleLogin = async (values: {
-        tenant: string;
-        userNameOrEmail: string;
-        password: string;
-        rememberMe: boolean;
-    }) => {
-        try {
-            const tenantName = values.tenant;
-            const tenantResult = await LoginService.CheckTenant(tenantName);
-            console.log(tenantResult);
-            if (tenantResult.tenantId !== null) {
-                loginModel.tenancyName = values.tenant;
-                loginModel.userNameOrEmailAddress = values.userNameOrEmail;
-                loginModel.password = values.password;
-                loginModel.rememberMe = values.rememberMe;
-                const loginResult = await LoginService.Login(loginModel);
-                if (loginResult) {
-                    navigate('/');
-                } else {
-                    form.setFields([
-                        {
-                            name: 'userNameOrEmail',
-                            errors: ['Tài khoản hoặc mật khẩu không chính xác!']
-                        },
-                        { name: 'password', errors: ['Tài khoản hoặc mật khẩu không chính xác!'] }
-                    ]);
-                }
-            } else {
-                form.setFields([{ name: 'tenant', errors: ['ID không chính xác'] }]);
-            }
-        } catch (error) {
-            console.error('An error occurred during login:', error);
-            form.setFields([
-                { name: 'userNameOrEmail', errors: ['Tài khoản hoặc mật khẩu không chính xác!'] },
-                { name: 'password', errors: ['Tài khoản hoặc mật khẩu không chính xác!'] }
-            ]);
-        }
-    };
-    useEffect(() => {
-        Object.keys(Cookies.get()).forEach((cookieName) => {
-            Cookies.remove(cookieName);
-        });
-    }, []);
-
     return (
         <div className=" login-page">
             <div className="logo-login">
@@ -64,78 +16,66 @@ const LoginScreen: React.FC = () => {
                 </div>
                 <div className="logo-text">Lucky Beauty</div>
             </div>
-            <Row className="align-items-center justify-content-center mt-2 h-100">
-                <Col>
+            <Grid container className="align-items-center justify-content-center mt-2 h-100">
+                <Grid xs={12}>
                     <div className="login-page-inner ">
                         <h1 className="login-label">Đăng nhập</h1>
-                        <Form
-                            form={form}
-                            onFinish={handleLogin}
-                            layout="vertical"
-                            className="login-form">
-                            <Form.Item
-                                name="tenant"
-                                label={<span className="login-label">ID cửa hàng</span>}>
-                                <Input size="large" placeholder="Nhập tên cửa hàng" />
-                            </Form.Item>
-                            <Form.Item
-                                name="userNameOrEmail"
-                                label={
-                                    <span className="login-label">
-                                        Tên đăng nhập <span style={{ color: 'red' }}>*</span>
-                                    </span>
-                                }
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Vui lòng nhập tài khoản'
-                                    }
-                                ]}>
-                                <Input size="large" placeholder="Nhập email hoặc tài khoản" />
-                            </Form.Item>
-                            <Form.Item
-                                name="password"
-                                label={
-                                    <span className="login-label">
-                                        Mật khẩu <span style={{ color: 'red' }}>*</span>
-                                    </span>
-                                }
-                                rules={[{ required: true, message: 'Vui lòng nhập mật khẩu' }]}>
-                                <Input.Password size="large" placeholder="Nhập mật khẩu" />
-                            </Form.Item>
-                            <Form.Item>
-                                <Checkbox
-                                    className="remember"
-                                    value={remember}
-                                    checked={remember}
-                                    onChange={() => {
-                                        setRemember(!remember);
-                                    }}>
-                                    Ghi nhớ
-                                </Checkbox>
+                        <form className="login-form">
+                            <Grid container>
+                                <Grid xs={12} className="form-item">
+                                    <TextField
+                                        name="tenant"
+                                        label={
+                                            <span className="login-label">ID đăng nhập</span>
+                                        }></TextField>
+                                </Grid>
+                                <Grid xs={12} className="form-item">
+                                    <TextField
+                                        name="userNameOrEmail"
+                                        label={<span className="login-label">Tên đăng nhập</span>}>
+                                        <Input placeholder="Nhập email hoặc tài khoản" />
+                                    </TextField>
+                                </Grid>
+                                <Grid xs={12} className="form-item">
+                                    <TextField
+                                        name="userNameOrEmail"
+                                        label={<span className="login-label">Mật khẩu</span>}>
+                                        <Input placeholder="Nhập email hoặc tài khoản" />
+                                    </TextField>
+                                </Grid>
+                                <Grid xs={12} className="form-item_checkBox">
+                                    <FormControlLabel
+                                        control={<Checkbox defaultChecked />}
+                                        label="Ghi nhớ"
+                                    />
+                                    <Link className="login-form-forgot" to="/forgot-password">
+                                        Quên mật khẩu ?
+                                    </Link>
+                                </Grid>
 
-                                <Link className="login-form-forgot" to="/forgot-password">
-                                    Quên mật khẩu ?
-                                </Link>
-                            </Form.Item>
-                            <Row>
-                                <button type="submit" className="btn-login">
-                                    <span className="text-login">Đăng nhập</span>
-                                </button>
-                            </Row>
-                            <p className="text-support">
-                                Tổng đài hỗ trợ : <span>0247 303 9333 - 0936 363 069</span>
-                            </p>
-                            <p className="text-register">
-                                Bạn chưa có tài khoản?{' '}
-                                <Link className="a quenMk" to="/register">
-                                    Đăng ký
-                                </Link>
-                            </p>
-                        </Form>
+                                <Grid xs={12}>
+                                    <button type="submit" className="btn-login">
+                                        <span className="text-login">Đăng nhập</span>
+                                    </button>
+                                </Grid>
+                                <Grid xs={12}>
+                                    <p className="text-support">
+                                        Tổng đài hỗ trợ : <span>0247 303 9333 - 0936 363 069</span>
+                                    </p>
+                                </Grid>
+                                <Grid xs={12}>
+                                    <p className="text-register">
+                                        Bạn chưa có tài khoản?{' '}
+                                        <Link className="a quenMk" to="/register">
+                                            Đăng ký
+                                        </Link>
+                                    </p>
+                                </Grid>
+                            </Grid>
+                        </form>
                     </div>
-                </Col>
-            </Row>
+                </Grid>
+            </Grid>
         </div>
     );
 };
