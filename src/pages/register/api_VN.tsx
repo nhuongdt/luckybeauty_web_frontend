a42d372f4acff185a1c33e293e93bf4a94393fe6
ducmanhssoft <ducmanh.ssoft@gmail.com>
2023-05-18 11:50:02 +0700

    fix get api_VN.tsx

--- src/pages/register/api_VN.tsx
+++ src/pages/register/api_VN.tsx
@@ -6,11 +6,15 @@ interface Province {
     codeName: string;
     code: number;
     name: string;
+    districts: District[];
 }
 interface District {
     codeName: string;
     name: string;
     code: number;
+    division_type: string;
+    province_code: number;
+    wards: [];
 }
 const ApiVN: React.FC = () => {
     const [provinces, setProvinces] = useState<Province[]>([]);
@@ -19,27 +23,27 @@ const ApiVN: React.FC = () => {
     const [selectedDistrict, setSelectedDistrict] = useState<string>('');
     useEffect(() => {
         axios
-            .get<Province[]>('https://provinces.open-api.vn/api/p/')
+            .get<Province[]>('https://provinces.open-api.vn/api/?depth=2')
             .then((response) => {
                 setProvinces(response.data);
             })
             .catch((error) => console.log(error));
     }, []);
 
-    useEffect(() => {
-        if (selectedProvince) {
-            axios
-                .get<District[]>(`https://provinces.open-api.vn/api/p/${selectedProvince}/?depth=2`)
-                .then((response) => {
-                    const districtList = response.data;
-                    setDistricts(districtList);
-                    console.log(districts);
-                })
-                .catch((error) => console.log(error));
-        } else {
-            setDistricts([]);
+    const handleProvinceChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+        const provinceCode = e.target.value as string;
+        setSelectedProvince(provinceCode);
+        const selectedProvinceObj = provinces.find(
+            (province) => province.code.toString() === provinceCode
+        );
+        if (selectedProvinceObj) {
+            setDistricts(selectedProvinceObj.districts);
         }
-    }, [selectedProvince]);
+    };
+
+    const handleDistrictChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+        setSelectedDistrict(e.target.value as string);
+    };
 
     return (
         <div className="select-row">
@@ -47,7 +51,7 @@ const ApiVN: React.FC = () => {
                 <InputLabel htmlFor="province-native-select">Tỉnh/Thành phố</InputLabel>
                 <NativeSelect
                     value={selectedProvince}
-                    onChange={(e) => setSelectedProvince(e.target.value)}
+                    onChange={handleProvinceChange}
                     inputProps={{
                         name: 'province',
                         id: 'province-native-select'
@@ -63,7 +67,8 @@ const ApiVN: React.FC = () => {
             <FormControl>
                 <InputLabel htmlFor="district-native-select">Quận/Huyện</InputLabel>
                 <NativeSelect
-                    onChange={(e) => setSelectedDistrict(e.target.value)}
+                    value={selectedDistrict}
+                    onChange={handleDistrictChange}
                     inputProps={{
                         name: 'district',
                         id: 'district-native-select'
