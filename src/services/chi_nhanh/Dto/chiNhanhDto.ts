7f68bc9688a1f87b4487ab6bfe95b133f4cd56b4
ducmanhssoft <ducmanh.ssoft@gmail.com>
2023-05-18 11:02:16 +0700

    add config file docker

---
+++ src/services/chi_nhanh/Dto/chiNhanhDto.ts
@@ -0,0 +1,13 @@
+export interface ChiNhanhDto {
+    idCongTy: string;
+    maChiNhanh: string;
+    tenChiNhanh: string;
+    soDienThoai: null;
+    diaChi: string;
+    maSoThue: string;
+    logo: null;
+    ghiChu: string;
+    trangThai: number;
+    ngayHetHan: Date;
+    ngayApDung: Date;
+}
