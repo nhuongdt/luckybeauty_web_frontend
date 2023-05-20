2d277faf46d8b32316e54acd66428708448d21ca
Tài Đinh <dinhtuantaiok@gmail.com>
2023-05-20 01:50:14 -0700

    label

---
+++ src/pages/ban_hang/CheckInNew.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+const CheckIn: React.FC = () => {
+    return <div></div>;
+};
+export default CheckIn;
--- src/pages/ban_hang/nhan_vien_thuc_hien.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-const ImplementationStaff: React.FC = () => {
-    return <div className="nhan_vien_thuc_hien"></div>;
-};
-export default ImplementationStaff;
