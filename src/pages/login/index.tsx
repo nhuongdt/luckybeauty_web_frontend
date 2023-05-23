06c814629e130f46defa8db32dca0dfb0558be09
Tài Đinh <dinhtuantaiok@gmail.com>
2023-05-22 20:08:51 -0700

    validate login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -24,6 +24,52 @@ const LoginScreen: React.FC = () => {
     const handleShowPassword = () => {
         setShowPassword(!showPassword);
     };
+    const [valId, setValId] = useState(true);
+    const [checkChangeID, setCheckChangeId] = useState(false);
+    const [id, setId] = useState('');
+    const [valName, setValName] = useState(true);
+    const [checkChangeName, setCheckChangeName] = useState(false);
+    const [name, setName] = useState('');
+    const [valPassword, setValPassword] = useState(true);
+    const [checkChangePassword, setCheckChangePassword] = useState(false);
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        if (id == '' || name == '' || password == '') {
+            alert('Vui lòng nhập đầy đủ thông tin Đăng nhập');
+        } else if ((id !== '' && name !== '') || password !== '') {
+            console.log('id:', id, 'name:', name, 'Password: ', password);
+        }
+    };
+    const handleChangeId = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setId(value);
+        setCheckChangeId(true);
+    };
+    const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setName(value);
+        setCheckChangeName(true);
+    };
+    const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setPassword(value);
+        setCheckChangePassword(true);
+    };
+    useEffect(() => {
+        if (checkChangeID == true) {
+            console.log(id);
+            id !== '' ? setValId(true) : setValId(false);
+        }
+        if (checkChangeName == true) {
+            name !== '' ? setValName(true) : setValName(false);
+        }
+        if (checkChangePassword == true) {
+            password !== '' ? setValPassword(true) : setValPassword(false);
+        }
+    }, [id, name, password]);
+
     return (
         <div className=" login-page">
             <div className="logo-login">
@@ -36,10 +82,12 @@ const LoginScreen: React.FC = () => {
                 <Grid xs={12}>
                     <div className="login-page-inner ">
                         <h1 className="login-label">Đăng nhập</h1>
-                        <form className="login-form">
+                        <form className="login-form" onSubmit={handleSubmit}>
                             <Grid container>
                                 <Grid xs={12} className="form-item">
                                     <TextField
+                                        value={id}
+                                        onChange={handleChangeId}
                                         variant="outlined"
                                         name="tenant"
                                         label={<span className="login-label">ID đăng nhập</span>}
@@ -47,28 +95,36 @@ const LoginScreen: React.FC = () => {
                                         sx={{
                                             '& .MuiOutlinedInput-root': {
                                                 '& fieldset': {
-                                                    border: 'none'
+                                                    border:
+                                                        valId == true
+                                                            ? 'none!important'
+                                                            : '1px solid red!important'
                                                 }
                                             }
                                         }}></TextField>
                                 </Grid>
                                 <Grid xs={12} className="form-item">
                                     <TextField
+                                        value={name}
+                                        onChange={handleChangeName}
                                         variant="outlined"
                                         name="userNameOrEmail"
                                         label={<span className="login-label">Tên đăng nhập</span>}
                                         sx={{
                                             '& .MuiOutlinedInput-root': {
                                                 '& fieldset': {
-                                                    border: 'none'
+                                                    border:
+                                                        valName == true
+                                                            ? 'none!important'
+                                                            : '1px solid red!important'
                                                 }
                                             }
-                                        }}>
-                                        <Input placeholder="Nhập email hoặc tài khoản" />
-                                    </TextField>
+                                        }}></TextField>
                                 </Grid>
                                 <Grid xs={12} className="form-item">
                                     <TextField
+                                        value={password}
+                                        onChange={handleChangePassword}
                                         className="bg-pw"
                                         name="userNameOrEmail"
                                         variant="outlined"
@@ -76,7 +132,10 @@ const LoginScreen: React.FC = () => {
                                         sx={{
                                             '& .MuiOutlinedInput-root': {
                                                 '& fieldset': {
-                                                    border: 'none'
+                                                    border:
+                                                        valPassword == true
+                                                            ? 'none!important'
+                                                            : '1px solid red!important'
                                                 }
                                             }
                                         }}
@@ -93,9 +152,7 @@ const LoginScreen: React.FC = () => {
                                                     </IconButton>
                                                 </InputAdornment>
                                             )
-                                        }}>
-                                        <Input placeholder="Nhập email hoặc tài khoản" />
-                                    </TextField>
+                                        }}></TextField>
                                 </Grid>
                                 <Grid xs={12} className="form-item_checkBox">
                                     <FormControlLabel
