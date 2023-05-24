31690e2195238dfe1f1ac5d45ae8016f4f83d5d2
ducmanhssoft <ducmanh.ssoft@gmail.com>
2023-05-24 14:29:35 +0700

    fix config

--- src/pages/customer/index.tsx
+++ src/pages/customer/index.tsx
@@ -14,7 +14,8 @@ import {
     Select,
     MenuItem,
     Menu,
-    FormControl
+    FormControl,
+    TablePagination
 } from '@mui/material';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import fileIcon from '../../images/file.svg';
@@ -27,7 +28,10 @@ import UploadIcon from '../../images/upload.svg';
 import AddIcon from '../../images/add.svg';
 import SearchIcon from '../../images/search-normal.svg';
 import { ReactComponent as DateIcon } from '../../images/calendar-5.svg';
-
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import InfoIcon from '@mui/icons-material/Info';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import khachHangService from '../../services/khach-hang/khachHangService';
 import { CreateOrEditKhachHangDto } from '../../services/khach-hang/dto/CreateOrEditKhachHangDto';
 import { pageLoadTime } from '../../lib/abp';
@@ -43,6 +47,9 @@ class Customer extends React.Component {
         curentPage: 0,
         keyword: '',
         totalItems: 0,
+        moreOpen: false,
+        anchorEl: null,
+        selectedRowId: null,
         createOrEditKhachHang: {} as CreateOrEditKhachHangDto
     };
     componentDidMount(): void {
@@ -138,44 +145,38 @@ class Customer extends React.Component {
             Quản lý khách hàng
         </Typography>
     ];
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [selectedRowId, setSelectedRowId] = useState(null);
 
-    const handleOpenMenu = (event: any, rowId: any) => {
-        setAnchorEl(event.currentTarget);
-        setSelectedRowId(rowId);
+    handleOpenMenu = (event: any, rowId: any) => {
+        this.setState({ anchorEl: event.currentTarget, selectedRowId: rowId });
     };
 
-    const handleCloseMenu = () => {
-        setAnchorEl(null);
-        setSelectedRowId(null);
+    handleCloseMenu = () => {
+        this.setState({ anchorEl: null, selectedRowId: null });
     };
 
-    const handleView = () => {
+    handleView = () => {
         // Handle View action
-        handleCloseMenu();
+        this.handleCloseMenu();
     };
 
-    const handleEdit = () => {
+    handleEdit = () => {
         // Handle Edit action
-        handleCloseMenu();
+        this.handleCloseMenu();
     };
 
-    const handleDelete = () => {
+    handleDelete = () => {
         // Handle Delete action
-        handleCloseMenu();
+        this.handleCloseMenu();
     };
-    const [open, setOpen] = useState(false);
-
-    const handleOpen = () => {
-        setOpen(true);
+    handleOpen = () => {
+        this.setState({ moreOpen: true });
     };
 
-    const handleClose = () => {
-        setOpen(false);
+    handleClose = () => {
+        this.setState({ moreOpen: false });
     };
 
-    const columns: GridColDef[] = [
+    columns: GridColDef[] = [
         { field: 'id', headerName: 'ID', width: 50 },
 
         {
@@ -242,13 +243,14 @@ class Customer extends React.Component {
                     aria-label="Actions"
                     aria-controls={`actions-menu-${params.row.id}`}
                     aria-haspopup="true"
-                    onClick={(event) => handleOpenMenu(event, params.row.id)}>
+                    onClick={(event) => {
+                        this.handleOpenMenu(event, params.row.id);
+                    }}>
                     <MoreHorizIcon />
                 </IconButton>
             )
         }
     ];
-
     render(): React.ReactNode {
         return (
             <Box
@@ -268,240 +270,11 @@ class Customer extends React.Component {
                             fontWeight="700"
                             lineHeight="32px"
                             marginTop="4px">
-                            Danh sách khách hàng
+                            Danh sách nhân viên
                         </Typography>
                     </Grid>
                     <Grid xs={12} md="auto" item display="flex" gap="8px" justifyContent="end">
-                        <Box className="form-search">
-    const rows = [
-        {
-            id: 1,
-            name: 'Võ Việt Hà',
-            phone: '0911290476',
-            group: 'Nhóm 1',
-            total: 200000,
-            source: 'Trực tiếp',
-            staff: '12/02/2022',
-            recentAppointment: 'Đang làm việc',
-            gender: 'Nam',
-            avatar: avatar
-        },
-        {
-            id: 1777,
-            name: 'Võ Việt Hà',
-            phone: 'Jon',
-            age: 35,
-            staff: 'Hà Nội',
-            position: 'Nhân viên',
-            recentAppointment: '12/02/2022',
-            State: 'Đang làm việc',
-            gender: 'Nam',
-            avatar: avatar
-        },
-        {
-            id: 10,
-            name: 'Võ Việt Hà',
-            phone: 'Jon',
-            age: 35,
-            location: 'Hà Nội',
-            position: 'Nhân viên',
-            join: '12/02/2022',
-            State: 'Đang làm việc',
-            gender: 'Nam',
-            avatar: avatar
-        },
-        {
-            id: 16,
-            name: 'Võ Việt Hà',
-            phone: 'Jon',
-            age: 35,
-            location: 'Hà Nội',
-            total: 'Nhân viên',
-            join: '12/02/2022',
-            State: 'Đang làm việc',
-            gender: 'Nam',
-            avatar: avatar
-        },
-
-        { id: 2, name: 'Lannister', phone: 'Cersei', age: 42, location: 'Hà Nội', avatar: avatar },
-        { id: 3, name: 'Lannister', phone: 'Jaime', age: 45, location: 'Hà Nội', avatar: avatar },
-        { id: 4, name: 'Stark', phone: 'Arya', age: 16, location: 'Hà Nội', avatar: avatar },
-        {
-            id: 5,
-            name: 'Targaryen',
-            phone: 'Daenerys',
-            age: null,
-            location: 'Hà Nội',
-            avatar: avatar
-        },
-        { id: 6, name: 'Melisandre', phone: null, age: 150, location: 'Hà Nội', avatar: avatar },
-        { id: 7, name: 'Clifford', phone: 'Ferrara', age: 44, location: 'Hà Nội', avatar: avatar },
-        { id: 8, name: 'Frances', phone: 'Rossini', age: 36, location: 'Hà Nội', avatar: avatar },
-        { id: 9, name: 'Roxie', phone: 'Harvey', age: 65, location: 'Hà Nội', avatar: avatar }
-    ];
-    const [toggle, setToggle] = React.useState(false);
-    const handleToggle = () => {
-        setToggle(!toggle);
-    };
-
-    return (
-        <Box
-            className="customer-page"
-            paddingLeft="2.2222222222222223vw"
-            paddingRight="2.2222222222222223vw"
-            paddingTop="1.5277777777777777vw">
-            <Grid container alignItems="center" justifyContent="space-between">
-                <Grid item xs={12} md="auto">
-                    <Breadcrumbs separator="›" aria-label="breadcrumb">
-                        {breadcrumbs}
-                    </Breadcrumbs>
-                    <Typography
-                        color="#0C050A"
-                        variant="h1"
-                        fontSize="24px"
-                        fontWeight="700"
-                        lineHeight="32px"
-                        marginTop="4px">
-                        Danh sách nhân viên
-                    </Typography>
-                </Grid>
-                <Grid xs={12} md="auto" item display="flex" gap="8px" justifyContent="end">
-                    <Box component="form" className="form-search">
-                        <TextField
-                            sx={{
-                                backgroundColor: '#FFFAFF',
-                                borderColor: '#CDC9CD'
-                            }}
-                            className="search-field"
-                            variant="outlined"
-                            type="search"
-                            placeholder="Tìm kiếm"
-                            InputProps={{
-                                startAdornment: (
-                                    <IconButton type="submit">
-                                        <img src={SearchIcon} />
-                                    </IconButton>
-                                )
-                            }}
-                        />
-                    </Box>
-                    <ButtonGroup
-                        variant="contained"
-                        sx={{ gap: '8px' }}
-                        className="rounded-4px resize-height">
-                        <Button
-                            className="border-color"
-                            variant="outlined"
-                            startIcon={<img src={DownloadIcon} />}
-                            sx={{
-                                textTransform: 'capitalize',
-                                fontWeight: '400',
-                                color: '#666466'
-                            }}>
-                            Nhập
-                        </Button>
-                        <Button
-                            className="border-color"
-                            variant="outlined"
-                            startIcon={<img src={UploadIcon} />}
-                            sx={{
-                                textTransform: 'capitalize',
-                                fontWeight: '400',
-                                color: '#666466',
-                                padding: '10px 16px',
-                                borderColor: '#E6E1E6'
-                            }}>
-                            Xuất
-                        </Button>
-                        <Button
-                            className="bg-main"
-                            onClick={handleToggle}
-                            variant="contained"
-                            startIcon={<img src={AddIcon} />}
-                            sx={{
-                                textTransform: 'capitalize',
-                                fontWeight: '400',
-                                minWidth: '173px'
-                            }}>
-                            Thêm khách hàng
-                        </Button>
-                    </ButtonGroup>
-                </Grid>
-            </Grid>
-            <div
-                className="customer-page_row-2"
-                style={{ height: 582, width: '100%', marginTop: '24px' }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    initialState={{
-                        pagination: {
-                            paginationModel: { page: 0, pageSize: 5 }
-                        }
-                    }}
-                    pageSizeOptions={[5, 10]}
-                    checkboxSelection
-                />
-                <Menu
-                    id={`actions-menu-${selectedRowId}`}
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleCloseMenu}
-                    sx={{ minWidth: '120px' }}>
-                    <MenuItem onClick={handleView}>
-                        <Typography
-                            color="#009EF7"
-                            fontSize="12px"
-                            variant="button"
-                            textTransform="unset"
-                            width="64px"
-                            fontWeight="400"
-                            marginRight="8px">
-                            View
-                        </Typography>
-                        <InfoIcon sx={{ color: '#009EF7' }} />
-                    </MenuItem>
-                    <MenuItem onClick={handleEdit}>
-                        <Typography
-                            color="#009EF7"
-                            fontSize="12px"
-                            variant="button"
-                            textTransform="unset"
-                            width="64px"
-                            fontWeight="400"
-                            marginRight="8px">
-                            Edit
-                        </Typography>
-                        <EditIcon sx={{ color: '#009EF7' }} />
-                    </MenuItem>
-                    <MenuItem onClick={handleDelete}>
-                        <Typography
-                            color="#F1416C"
-                            fontSize="12px"
-                            variant="button"
-                            textTransform="unset"
-                            width="64px"
-                            fontWeight="400"
-                            marginRight="8px">
-                            Delete
-                        </Typography>
-                        <DeleteForeverIcon sx={{ color: '#F1416C' }} />
-                    </MenuItem>
-                </Menu>
-            </div>
-            <div
-                className={toggle ? 'show customer-overlay' : 'customer-overlay'}
-                onClick={handleToggle}></div>
-            <div className={toggle ? 'show poppup-add' : 'poppup-add'}>
-                <div className="poppup-title">Thêm khách hàng mới</div>
-                <div className="poppup-des">Thông tin chi tiết</div>
-                <Box component="form" className="form-add">
-                    <Grid container className="form-container" spacing={2}>
-                        <Grid item xs={12}>
-                            <Typography color="#4C4B4C" variant="subtitle2">
-                                Họ và tên
-                            </Typography>
+                        <Box component="form" className="form-search">
                             <TextField
                                 sx={{
                                     backgroundColor: '#FFFAFF',
@@ -509,19 +282,11 @@ class Customer extends React.Component {
                                 }}
                                 className="search-field"
                                 variant="outlined"
-                                type="text"
-                                onChange={(e) => {
-                                    this.setState({ keyword: e.target.value });
-                                }}
+                                type="search"
                                 placeholder="Tìm kiếm"
                                 InputProps={{
                                     startAdornment: (
-                                        <IconButton
-                                            type="button"
-                                            onClick={() => {
-                                                this.setState({ skipCount: 0 });
-                                                this.getData();
-                                            }}>
+                                        <IconButton type="submit">
                                             <img src={SearchIcon} />
                                         </IconButton>
                                     )
@@ -546,9 +311,6 @@ class Customer extends React.Component {
                             <Button
                                 className="border-color"
                                 variant="outlined"
-                                onClick={() => {
-                                    this.exportToExcel();
-                                }}
                                 startIcon={<img src={UploadIcon} />}
                                 sx={{
                                     textTransform: 'capitalize',
@@ -561,9 +323,7 @@ class Customer extends React.Component {
                             </Button>
                             <Button
                                 className="bg-main"
-                                onClick={() => {
-                                    this.createOrUpdateModalOpen('');
-                                }}
+                                onClick={this.handleToggle}
                                 variant="contained"
                                 startIcon={<img src={AddIcon} />}
                                 sx={{
@@ -595,6 +355,53 @@ class Customer extends React.Component {
                         pageSizeOptions={[5, 10]}
                         checkboxSelection
                     />
+                    <Menu
+                        id={`actions-menu-${this.state.selectedRowId}`}
+                        anchorEl={this.state.anchorEl}
+                        keepMounted
+                        open={Boolean(this.state.anchorEl)}
+                        onClose={this.handleCloseMenu}
+                        sx={{ minWidth: '120px' }}>
+                        <MenuItem onClick={this.handleView}>
+                            <Typography
+                                color="#009EF7"
+                                fontSize="12px"
+                                variant="button"
+                                textTransform="unset"
+                                width="64px"
+                                fontWeight="400"
+                                marginRight="8px">
+                                View
+                            </Typography>
+                            <InfoIcon sx={{ color: '#009EF7' }} />
+                        </MenuItem>
+                        <MenuItem onClick={this.handleEdit}>
+                            <Typography
+                                color="#009EF7"
+                                fontSize="12px"
+                                variant="button"
+                                textTransform="unset"
+                                width="64px"
+                                fontWeight="400"
+                                marginRight="8px">
+                                Edit
+                            </Typography>
+                            <EditIcon sx={{ color: '#009EF7' }} />
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDelete}>
+                            <Typography
+                                color="#F1416C"
+                                fontSize="12px"
+                                variant="button"
+                                textTransform="unset"
+                                width="64px"
+                                fontWeight="400"
+                                marginRight="8px">
+                                Delete
+                            </Typography>
+                            <DeleteForeverIcon sx={{ color: '#F1416C' }} />
+                        </MenuItem>
+                    </Menu>
                     <TablePagination
                         rowsPerPageOptions={[10, 25, 50]}
                         component="div"
