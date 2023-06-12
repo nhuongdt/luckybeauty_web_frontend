db73076df642d8d385ece3b1e96c4ecd45bdbf99
Tài Đinh <dinhtuantaiok@gmail.com>
2023-06-12 16:58:03 +0700

    update 2 poppup lịch làm việc nhân viên

--- src/pages/employee/lich-lam-viec/index.tsx
+++ src/pages/employee/lich-lam-viec/index.tsx
@@ -1,11 +1,27 @@
 import { Box, Typography, Grid, Button } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import ThemLich from './them_lich_lam_viec';
+import Delete from './deleteAlert';
 const LichLamViec: React.FC = () => {
+    const [openDialog, setOpenDialog] = useState(false);
+    const handleOpenDialog = () => {
+        setOpenDialog(true);
+    };
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
+    const [openDelete, setOpenDelete] = useState(false);
+    const handleOpenDelete = () => {
+        setOpenDelete(true);
+    };
+    const handleCloseDelete = () => {
+        setOpenDelete(false);
+    };
     return (
         <Box>
-            <ThemLich />
+            <ThemLich open={openDialog} onClose={handleCloseDialog} />
             <Box sx={{ padding: '16px 2.2222222222222223vw' }}>
                 <Grid container alignItems="center" justifyContent="space-between">
                     <Grid item xs="auto">
@@ -18,7 +34,10 @@ const LichLamViec: React.FC = () => {
                             <Button>
                                 <MoreHorizIcon sx={{ color: '#231F20' }} />
                             </Button>
-                            <Button variant="contained" sx={{ bgcolor: '#7C3367!important' }}>
+                            <Button
+                                onClick={handleOpenDialog}
+                                variant="contained"
+                                sx={{ bgcolor: '#7C3367!important' }}>
                                 Thêm ca
                             </Button>
                         </Box>
