eabbc9a1e6915f2e221f48a504ef167318f674e6
NhuongDT <inftech.ts91@gmail.com>
2023-06-14 09:16:27 +0700

    test (html-react-parser)

--- src/components/Print/ComponentToPrint.tsx
+++ src/components/Print/ComponentToPrint.tsx
@@ -1,87 +1,97 @@
 import React from 'react';
-import { useState, forwardRef, createContext } from 'react';
-import { PropToChildMauIn } from '../../utils/PropParentToChild';
-const PrintContext = createContext(null);
+import { useState, forwardRef, useEffect } from 'react';
+import parse, {
+    domToReact,
+    attributesToProps,
+    htmlToDOM,
+    HTMLReactParserOptions,
+    Element
+} from 'html-react-parser';
+import { CommentSharp } from '@mui/icons-material';
 
-// export const ComponentToPrint = forwardRef(function ComponentToPrint({ props, ref }: any) {
-//     return <div ref={ref} dangerouslySetInnerHTML={{ __html: ref }}></div>;
-// });
+export const ComponentToPrint = forwardRef(function ComponentToPrint({ props }: any, ref: any) {
+    const [contentHtml, setContentHtml] = useState('');
+    // const [hdPrint, setHdPrint] = useState('');
+    // const [cthdPrint, setCthdPrint] = useState('');
 
-export const ContenPrint = ({ child }: any) => {
-    return <>{child}</>;
-};
+    const [arrPropCTHD, setArrPropCTHD] = useState<string[]>([]);
 
-export const ComponentToPrint = ({ props }: any) => {
-    const [isShow, setIsShow] = useState(false);
-    const [propMauIn, setPropMauIn] = useState(null);
-    const [contentData, setContentData] = React.useState();
-
-    React.useEffect(() => {
-        if (props.contentHtml) {
-            let contentHtml = props.contentHtml;
-            // contentHtml = ReplaceHoaDon(contentHtml);
-            // contentHtml = ReplaceKhachHang(contentHtml);
-            contentHtml = ReplaceHoaDonChiTiet(contentHtml);
-            setContentData(contentHtml);
-            setIsShow(true);
+    useEffect(() => {
+        if (props?.hoadonChiTiet.length > 0) {
+            // get all properties of hoadonchitiet
+            setArrPropCTHD(Object.getOwnPropertyNames(props?.hoadonChiTiet[0]));
         }
-    }, [props.contentHtml]);
+        let html = props?.contentHtml;
+        html = ReplaceHoaDon(html);
+        html = ReplaceHoaDonChiTiet(html);
+        setContentHtml(html);
+        console.log('parserA2 ', props?.hoadonChiTiet);
+    }, [props?.contentHtml]);
 
+    const parserA = (input: string) =>
+        parse(input, {
+            replace: (domNode) => {
+                if (domNode instanceof Element && domNode.attribs.id === 'tblHangHoa') {
+                    return (
+                        <tbody>
+                            {props?.hoadonChiTiet.map((ct: any, index: any) => (
+                                <>
+                                    <tr></tr>
+                                </>
+                            ))}
+                        </tbody>
+                    );
+                }
+            }
+        });
+
+    const options = {
+        replace: ({ attribs, children }: any) => {
+            if (!attribs) {
+                return;
+            }
+
+            if (attribs.id === 'tblHangHoa') {
+                console.log('options ', document.getElementById('tblHangHoa')?.innerHTML);
+                return (
+                    <tbody>
+                        {props?.hoadonChiTiet.map((ct: any, index: any) => {
+                            document
+                                .getElementById('tblHangHoa')
+                                ?.innerHTML?.replace('{TenHangHoa}', `${ct.tenHangHoa}`)
+                                .replace('{GiaBan}', `${ct.giaBan}`) ?? '';
+                        })}
+                    </tbody>
+                );
+            }
+        }
+    };
+
+    const ReplaceHoaDon = (str: string) => {
+        str = str.replace('{MaHoaDon}', `${props?.hoadon.tongTienHang}`);
+        str = str.replace('{TenKhachHang}', `${props?.khachhang?.tenKhachHang}`);
+        return str;
+    };
     const ReplaceHoaDonChiTiet = (str: string) => {
         str = str.replace('{TenHangHoa}', '{ct.tenHangHoa}');
-        str = str.replace('{SoLuong}', '{ct.soLuong}');
-        str = str.replace('{GiaBan}', '{ct.giaBan}');
-        str = str.replace('{ThanhTien}', '{ct.thanhTien}');
+        // for (let i = 0; i < props?.hoadonChiTiet.length; i++) {
+        //     const ct = props?.hoadonChiTiet[i];
+        //     if (i > 0) {
+        //         str = str.concat(`${ct.tenHangHoa}`);
+        //     } else {
+        //         str = str.replace('{TenHangHoa}', `${ct.tenHangHoa}`);
+        //     }
+        //  }
+
         return str;
     };
 
     return (
         <>
-            <PrintContext.Provider value={propMauIn}>
-                <div className={isShow ? 'show overlay' : 'overlay'}></div>
-                <div className={isShow ? 'show ' : ''}>
-                    <ContenPrint child={contentData} />
-                </div>
-            </PrintContext.Provider>
+            {/* <div dangerouslySetInnerHTML={{ __html: content }} /> */}
+            {/* <div ref={ref}>{parse(`${contentHtml}`)}</div> */}
+            {/* <div ref={ref}>{parse(contentHtml, options)}</div> */}
+            <div ref={ref}>{parserA(contentHtml)}</div>
         </>
     );
-};
-
-// import CKEditor from '@ckeditor/ckeditor5-react';
-// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
-// const CKEditorComponent = () => {
-//   const [content, setContent] = useState('');
-
-//   const handleEditorChange = (event, editor) => {
-//     const data = editor.getData();
-//     setContent(data);
-//   };
-
-//   return (
-//     <div>
-//       <CKEditor
-//         editor={ClassicEditor}
-//         data={content}
-//         onChange={handleEditorChange}
-//       />
-//       <div>
-//         <h3>Content:</h3>
-//         <div dangerouslySetInnerHTML={{ __html: content }} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export  CKEditorComponent;
-
-// const App = () => {
-//     return (
-//       <div>
-//         <h1>CKEditor Example</h1>
-//         <CKEditorComponent />
-//       </div>
-//     );
-//   };
-
-//   export default App;
+});
