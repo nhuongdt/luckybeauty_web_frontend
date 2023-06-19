33385f70cdf7709f87527225fc4e0dca5348cc39
NhuongDT <inftech.ts91@gmail.com>
2023-06-19 15:45:28 +0700

    sửa tên Interface (chưa dùng đến)

--- src/services/dto/InputFormat.ts
+++ src/services/dto/InputFormat.ts
@@ -1,8 +1,17 @@
-export interface InputNumber {
+export interface IInputNumber {
     textMask: string;
-    numberFormat: string;
+    numberValue: string;
 }
 export interface InputPropNumber {
     onChange: (event: { target: { name: string; value: string } }) => void;
     name: string;
+}
+
+export class ClassInputNumber implements IInputNumber {
+    textMask = '000-0000';
+    numberValue: string;
+    constructor({ textMask = '000-0000', numberValue = '0' }) {
+        this.textMask = textMask;
+        this.numberValue = numberValue;
+    }
 }
