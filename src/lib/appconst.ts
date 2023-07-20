5bd205d361dc8bc5e88a2fbf1d849a7922540ed4
NhuongDT <inftech.ts91@gmail.com>
2023-07-20 11:33:18 +0700

    hinhThucThanhToan: {id, text} --> {value, text}

--- src/lib/appconst.ts
+++ src/lib/appconst.ts
@@ -2,6 +2,11 @@ const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 const phoneRegex = /(84[1-9]|0[1-9])+([0-9]{8})\b/g;
 const yyyyMMddHHmmRegex = /^\d{4}-[0-1][0-2]-[0-3]\d\s([0-1][0-9]|2[0-3]):[0-5]\d$/;
 
+export interface ISelect {
+    value: number;
+    text: string;
+}
+
 const AppConsts = {
     userManagement: {
         defaultAdminUserName: 'admin'
@@ -26,10 +31,10 @@ const AppConsts = {
         { value: 4, name: 'Xóa' }
     ],
     hinhThucThanhToan: [
-        { id: 1, text: 'Tiền mặt' },
-        { id: 2, text: 'Chuyển khoản' },
-        { id: 3, text: 'Quẹt thẻ' }
-    ],
+        { value: 1, text: 'Tiền mặt' },
+        { value: 2, text: 'Chuyển khoản' },
+        { value: 3, text: 'Quẹt thẻ' }
+    ] as ISelect[],
     loaiBooking: {
         online: 0,
         offline: 1,
