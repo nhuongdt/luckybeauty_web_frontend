e299cad7e52252e24c37b40b8d5d5e2ac81de0cf
NhuongDT <inftech.ts91@gmail.com>
2023-08-04 15:50:32 +0700

    tui chịu rồi, up lên để thử cái #

--- craco.config.js
+++ craco.config.js
@@ -1,23 +1,22 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
+
 'use strict';
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
+const svgRegex = /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/;
+const cssEditorRegex = /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/;
+const { getLoader, loaderByName, addBeforeLoader } = require('@craco/craco');
 const { styles } = require('@ckeditor/ckeditor5-dev-utils');
 module.exports = {
-    overrideWebpackConfig: ({
-        webpackConfig,
-        cracoConfig,
-        pluginOptions,
-        context: { env, paths }
-    }) => {
+    overrideWebpackConfig: ({ webpackConfig }) => {
         // start: (add new rule) for SVG and CSS loaders
         const svgLoader = {
-            test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+            test: svgRegex,
             use: ['raw-loader']
         };
         const cssLoader = {
-            test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+            test: cssEditorRegex,
             use: [
                 {
                     loader: 'style-loader',
@@ -44,45 +43,53 @@ module.exports = {
         };
         addBeforeLoader(webpackConfig, loaderByName('file-loader'), svgLoader);
         addBeforeLoader(webpackConfig, loaderByName('file-loader'), cssLoader);
+
         // end (add new rule)
 
         // update rule (find by name)
-        const { cssFind, match: cssLoaderMatch } = getLoader(webpackConfig, loaderByName(cssRegex));
-        if (!cssFind) {
-            throwUnexpectedConfigError({
-                message: `Can't find file-loader in the ${context.env} webpack config!`
-            });
-        }
-        cssLoaderMatch.loader.exclude.push(/ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/);
-
-        const { cssModuleFind, match: cssModuleLoaderMatch } = getLoader(
-            webpackConfig,
-            loaderByName(cssModuleRegex)
-        );
-        if (!cssModuleFind) {
-            throwUnexpectedConfigError({
-                message: `Can't find file-loader in the ${context.env} webpack config!`
-            });
-        }
-        cssModuleLoaderMatch.loader.exclude.push(/ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/);
+        webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
+            if (String(rule.test) === String(cssRegex)) {
+                return {
+                    ...rule,
+                    exclude: [cssModuleRegex, cssEditorRegex]
+                };
+            }
+            return rule;
+        });
+        webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
+            if (String(rule.test) === String(cssModuleRegex)) {
+                return {
+                    ...rule,
+                    exclude: [cssEditorRegex]
+                };
+            }
+            return rule;
+        });
 
         const { isFound, match: fileLoaderMatch } = getLoader(
             webpackConfig,
             loaderByName('file-loader')
         );
-
-        if (!isFound) {
-            throwUnexpectedConfigError({
-                message: `Can't find file-loader in the ${context.env} webpack config!`
-            });
+        if (isFound) {
+            fileLoaderMatch.loader.exclude.push(/\.(js|mjs|jsx|ts|tsx)$/);
+            fileLoaderMatch.loader.exclude.push(/\.html$/);
+            fileLoaderMatch.loader.exclude.push(/\.json$/);
+            fileLoaderMatch.loader.exclude.push(svgRegex);
+            fileLoaderMatch.loader.exclude.push(cssEditorRegex);
         }
-        fileLoaderMatch.loader.exclude.push([
-            /\.(js|mjs|jsx|ts|tsx)$/,
-            /\.html$/,
-            /\.json$/,
-            /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-            /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/
-        ]);
+
+        // fileLoaderMatch.forEach(({ loader }) => {
+        //     loader.options = {
+        //         name: 'static/media/[name].[hash].[ext]',
+        //         exclude: [
+        //             /\.(js|mjs|jsx|ts|tsx)$/,
+        //             /\.html$/,
+        //             /\.json$/,
+        //             svgRegex,
+        //             cssEditorRegex
+        //         ]
+        //     };
+        // });
         return webpackConfig;
     }
 };
