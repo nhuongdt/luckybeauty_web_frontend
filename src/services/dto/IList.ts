0a4a27d0dd453ec5518948fbe82f07e171e6a5f5
NhuongDT <inftech.ts91@gmail.com>
2023-09-06 14:37:29 +0700

    interface IList: add prop (icon?: ReactComponentElement<SvgIconComponent>)

--- src/services/dto/IList.ts
+++ src/services/dto/IList.ts
@@ -1,5 +1,9 @@
+import { SvgIconComponent } from '@mui/icons-material';
+import { ReactComponentElement } from 'react';
+
 export interface IList {
     id: string;
     text: string;
     color?: string;
+    icon?: ReactComponentElement<SvgIconComponent>;
 }
