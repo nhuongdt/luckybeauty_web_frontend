a02c97a781a7bb73b3dc612a0c5cb3c8fec2176b
NhuongDT <inftech.ts91@gmail.com>
2023-10-10 08:18:21 +0700

    ActionViewEditDelete: add {lstOption}

--- src/components/Menu/ActionViewEditDelete.tsx
+++ src/components/Menu/ActionViewEditDelete.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { IconButton, Menu, MenuItem, Typography } from '@mui/material';
 import { Info, Edit, DeleteForever, MoreHoriz } from '@mui/icons-material';
+import { IList } from '../../services/dto/IList';
 
-export default function ActionViewEditDelete({ handleAction }: any) {
+export default function ActionViewEditDelete({ handleAction, lstOption }: any) {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -23,51 +24,76 @@ export default function ActionViewEditDelete({ handleAction }: any) {
                 onClick={handleClick}>
                 <MoreHoriz />
             </IconButton>
+
             <Menu
                 id="demo-positioned-menu"
                 aria-labelledby="demo-positioned-button"
                 anchorEl={anchorEl}
                 open={open}
                 onClose={() => setAnchorEl(null)}>
-                <MenuItem onClick={() => handleClickAction(0)}>
-                    <Typography
-                        color="#009EF7"
-                        fontSize="12px"
-                        variant="button"
-                        textTransform="unset"
-                        width="64px"
-                        fontWeight="400"
-                        marginRight="8px">
-                        Xem
-                    </Typography>
-                    <Info sx={{ color: '#009EF7' }} />
-                </MenuItem>
-                <MenuItem onClick={() => handleClickAction(1)}>
-                    <Typography
-                        color="#009EF7"
-                        fontSize="12px"
-                        variant="button"
-                        textTransform="unset"
-                        width="64px"
-                        fontWeight="400"
-                        marginRight="8px">
-                        Sửa
-                    </Typography>
-                    <Edit sx={{ color: '#009EF7' }} />
-                </MenuItem>
-                <MenuItem onClick={() => handleClickAction(2)}>
-                    <Typography
-                        color="#F1416C"
-                        fontSize="12px"
-                        variant="button"
-                        textTransform="unset"
-                        width="64px"
-                        fontWeight="400"
-                        marginRight="8px">
-                        Xóa
-                    </Typography>
-                    <DeleteForever sx={{ color: '#F1416C' }} />
-                </MenuItem>
+                {lstOption && lstOption.length > 0 ? (
+                    <div>
+                        {lstOption?.map((item: IList, index: number) => (
+                            <MenuItem
+                                onClick={() => handleClickAction(parseInt(item.id))}
+                                key={index}>
+                                <Typography
+                                    color={item?.color}
+                                    fontSize="12px"
+                                    variant="button"
+                                    textTransform="unset"
+                                    width="64px"
+                                    fontWeight="400"
+                                    marginRight="8px">
+                                    {item.text}
+                                </Typography>
+                                {item.icon}
+                            </MenuItem>
+                        ))}
+                    </div>
+                ) : (
+                    <div>
+                        <MenuItem onClick={() => handleClickAction(0)}>
+                            <Typography
+                                color="#009EF7"
+                                fontSize="12px"
+                                variant="button"
+                                textTransform="unset"
+                                width="64px"
+                                fontWeight="400"
+                                marginRight="8px">
+                                Xem
+                            </Typography>
+                            <Info sx={{ color: '#009EF7' }} />
+                        </MenuItem>
+                        <MenuItem onClick={() => handleClickAction(1)}>
+                            <Typography
+                                color="#009EF7"
+                                fontSize="12px"
+                                variant="button"
+                                textTransform="unset"
+                                width="64px"
+                                fontWeight="400"
+                                marginRight="8px">
+                                Sửa
+                            </Typography>
+                            <Edit sx={{ color: '#009EF7' }} />
+                        </MenuItem>
+                        <MenuItem onClick={() => handleClickAction(2)}>
+                            <Typography
+                                color="#F1416C"
+                                fontSize="12px"
+                                variant="button"
+                                textTransform="unset"
+                                width="64px"
+                                fontWeight="400"
+                                marginRight="8px">
+                                Xóa
+                            </Typography>
+                            <DeleteForever sx={{ color: '#F1416C' }} />
+                        </MenuItem>
+                    </div>
+                )}
             </Menu>
         </div>
     );
