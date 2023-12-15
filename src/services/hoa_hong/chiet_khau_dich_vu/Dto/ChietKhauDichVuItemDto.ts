66b4d60815ea5cf98f48be8e00c5cf4ed7a317e5
NhuongDT <inftech.ts91@gmail.com>
2023-12-15 11:54:25 +0700

    add interface: ChietKhauDichVuItemDto_TachRiengCot

--- src/services/hoa_hong/chiet_khau_dich_vu/Dto/ChietKhauDichVuItemDto.ts
+++ src/services/hoa_hong/chiet_khau_dich_vu/Dto/ChietKhauDichVuItemDto.ts
@@ -7,4 +7,20 @@ export interface ChietKhauDichVuItemDto {
     hoaHongTuVan: number;
     giaDichVu: number;
     laPhanTram: boolean;
+    tenNhanVien: string;
+}
+
+export interface ChietKhauDichVuItemDto_TachRiengCot {
+    idNhanVien: string;
+    idDonViQuiDoi: string;
+    tenNhanVien: string;
+    tenDichVu: string;
+    tenNhomDichVu: string;
+    hoaHongThucHien: number;
+    hoaHongYeuCauThucHien: number;
+    hoaHongTuVan: number;
+    giaDichVu: number;
+    laPhanTram_HoaHongThucHien: boolean;
+    laPhanTram_HoaHongYeuCauThucHien: boolean;
+    laPhanTram_HoaHongTuVan: boolean;
 }
