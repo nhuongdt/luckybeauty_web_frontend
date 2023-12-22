e70e30e7319a7a225b0a71ef8410cf8d6f10361f
NhuongDT <inftech.ts91@gmail.com>
2023-12-22 16:12:45 +0700

    add interface INhatKyChuyenTienDto  extends ILichSuNap_ChuyenTienDto

--- src/services/sms/lich_su_nap_tien/ILichSuNap_ChuyenTienDto.ts
+++ src/services/sms/lich_su_nap_tien/ILichSuNap_ChuyenTienDto.ts
@@ -1,9 +1,16 @@
 export default interface ILichSuNap_ChuyenTienDto {
     tenantId: number;
+    id: string;
     idPhieuNapTien: string;
-    thoiGianNap_ChuyenTien: number;
+    thoiGianNap_ChuyenTien: string;
     idNguoiChuyenTien?: number;
     idNguoiNhanTien?: number;
     soTienChuyen_Nhan: number;
     noiDungChuyen_Nhan: string;
+}
+export interface INhatKyChuyenTienDto extends ILichSuNap_ChuyenTienDto {
+    userChuyenTien: string;
+    userNhanTien: string;
+    createTime: string;
+    loaiPhieu: string;
 }
