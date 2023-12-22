0fd1a792651b195dc3c830161cba46f80d73e712
NhuongDT <inftech.ts91@gmail.com>
2023-12-22 17:26:58 +0700

    IList: add {isShow}

--- src/services/dto/IList.ts
+++ src/services/dto/IList.ts
@@ -4,6 +4,7 @@ import { ReactComponentElement } from 'react';
 export interface IList {
     id: string;
     text: string;
+    isShow?: boolean;
     color?: string;
     icon?: ReactComponentElement<SvgIconComponent>;
 }
