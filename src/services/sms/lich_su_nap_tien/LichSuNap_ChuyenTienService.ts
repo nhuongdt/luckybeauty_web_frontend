d892d47388b92825a830abff7b6df7ec576b5e0d
NhuongDT <inftech.ts91@gmail.com>
2023-12-22 16:13:09 +0700

    add function: ThemMoiPhieuChuyenTien, XoaLichSuNapTien_byId, GetAllNhatKyChuyenTien

--- src/services/sms/lich_su_nap_tien/LichSuNap_ChuyenTienService.ts
+++ src/services/sms/lich_su_nap_tien/LichSuNap_ChuyenTienService.ts
@@ -1,7 +1,9 @@
 import Cookies from 'js-cookie';
 import http from '../../httpService';
-import ILichSuNap_ChuyenTienDto from './ILichSuNap_ChuyenTienDto';
+import ILichSuNap_ChuyenTienDto, { INhatKyChuyenTienDto } from './ILichSuNap_ChuyenTienDto';
 import utils from '../../../utils/utils';
+import { PagedResultDto } from '../../dto/pagedResultDto';
+import { ParamSearchDto } from '../../dto/ParamSearchDto';
 
 class LichSuNap_ChuyenTienService {
     ThemMoi_CapNhatPhieuChuyenTien = async (tenantId: number, input: ILichSuNap_ChuyenTienDto) => {
@@ -9,6 +11,10 @@ class LichSuNap_ChuyenTienService {
             `api/services/app/LichSuNap_ChuyenTien/ThemMoi_CapNhatPhieuChuyenTien?tenantId=${tenantId}`,
             input
         );
+        return result.data.result;
+    };
+    ThemMoiPhieuChuyenTien = async (input: ILichSuNap_ChuyenTienDto): Promise<ILichSuNap_ChuyenTienDto> => {
+        const result = await http.post(`api/services/app/LichSuNap_ChuyenTien/ThemMoiPhieuChuyenTien`, input);
         return result.data.result;
     };
     ThemMoi_CapNhatPhieuNapTien = async (tenantId: number, input: ILichSuNap_ChuyenTienDto) => {
@@ -27,6 +33,21 @@ class LichSuNap_ChuyenTienService {
             return result.data.result;
         }
         return 0;
+    };
+    XoaLichSuNapTien_byId = async (idPhieuNapTien: string): Promise<boolean> => {
+        if (!utils.checkNull(idPhieuNapTien)) {
+            const result = await http.get(
+                `api/services/app/LichSuNap_ChuyenTien/XoaLichSuNapTien_byId?id=${idPhieuNapTien}`
+            );
+            return result.data.result;
+        }
+        return false;
+    };
+    GetAllNhatKyChuyenTien = async (input: ParamSearchDto): Promise<PagedResultDto<INhatKyChuyenTienDto>> => {
+        const result = await http.get(`api/services/app/LichSuNap_ChuyenTien/GetAllNhatKyChuyenTien`, {
+            params: { ...input }
+        });
+        return result.data.result;
     };
 }
 
