2927f599b270e174594e8665e260f3a6259d7232
NhuongDT <inftech.ts91@gmail.com>
2024-01-09 14:31:12 +0700

    GetInforBooking_byID: return data

--- src/services/dat-lich/datLichService.ts
+++ src/services/dat-lich/datLichService.ts
@@ -1,6 +1,6 @@
 import http from '../httpService';
 import { BookingDto } from './dto/BookingDto';
-import { BookingGetAllItemDto } from './dto/BookingGetAllItemDto';
+import { BookingDetail_ofCustomerDto, BookingGetAllItemDto } from './dto/BookingGetAllItemDto';
 import { CreateBookingDto } from './dto/CreateBookingDto';
 import qs from 'qs';
 import { BookingRequestDto, PagedBookingResultRequestDto } from './dto/PagedBookingResultRequestDto';
@@ -31,8 +31,8 @@ class BookingServices {
         const response = await http.get(`api/services/app/Booking/GetForEdit?id=${id}`);
         return response.data.result;
     }
-    GetInforBooking_byID = async (idBooking: string) => {
-        if (utils.checkNull(idBooking) || idBooking == Guid.EMPTY) return null;
+    GetInforBooking_byID = async (idBooking: string): Promise<BookingDetail_ofCustomerDto[]> => {
+        if (utils.checkNull(idBooking) || idBooking == Guid.EMPTY) return [];
         const xx = await http.get(`api/services/app/Booking/GetInforBooking_byID?idBooking=${idBooking}`);
         return xx.data.result;
     };
