c7d5d0cabc92b26cb8173099cb2602435c0fcd7e
NhuongDT <inftech.ts91@gmail.com>
2024-01-10 11:15:31 +0700

    add func: ChucVu_GetDetail_byId + ChucVu_Delete

--- src/services/nhan-vien/chuc_vu/chucVuService.ts
+++ src/services/nhan-vien/chuc_vu/chucVuService.ts
@@ -2,9 +2,17 @@ import http from '../../httpService';
 import { CreateOrEditChucVuDto } from './dto/CreateOrEditChucVuDto';
 
 class ChucVuService {
-    async CreateOrEditChucVu(input: CreateOrEditChucVuDto) {
+    async CreateOrEditChucVu(input: CreateOrEditChucVuDto): Promise<CreateOrEditChucVuDto> {
         const response = await http.post(`/api/services/app/ChucVu/CreateOrEdit`, input);
         return response.data.result;
     }
+    ChucVu_GetDetail_byId = async (idChucVu: string): Promise<CreateOrEditChucVuDto> => {
+        const response = await http.get(`/api/services/app/ChucVu/GetDetail?id=${idChucVu}`);
+        return response.data.result;
+    };
+    ChucVu_Delete = async (idChucVu: string): Promise<CreateOrEditChucVuDto> => {
+        const response = await http.get(`/api/services/app/ChucVu/Delete?id=${idChucVu}`);
+        return response.data.result;
+    };
 }
 export default new ChucVuService();
