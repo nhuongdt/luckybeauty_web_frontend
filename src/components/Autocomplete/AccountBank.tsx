a77122be4cc1ea7636dd6fb673f501a227917fea
NhuongDT <inftech.ts91@gmail.com>
2024-02-19 17:04:11 +0700

    AutocompleteAccountBank: binf {logoNganHang}

--- src/components/Autocomplete/AccountBank.tsx
+++ src/components/Autocomplete/AccountBank.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Autocomplete, Button, Grid, TextField, Typography, Box } from '@mui/material';
+import { Autocomplete, Button, Grid, TextField, Typography, Box, Stack } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { TaiKhoanNganHangDto } from '../../services/so_quy/Dto/TaiKhoanNganHangDto';
 
 export default function AutocompleteAccountBank({
     handleChoseItem,
     idChosed,
-    listOption,
+    listOption = [],
     handleClickBtnAdd,
     roleAdd = false
 }: any) {
     const [itemChosed, setItemChosed] = useState<TaiKhoanNganHangDto | null>(null);
     React.useEffect(() => {
-        const item = listOption.filter((x: TaiKhoanNganHangDto) => x.id == idChosed);
+        const item = listOption?.filter((x: TaiKhoanNganHangDto) => x.id == idChosed);
         if (item.length > 0) {
             setItemChosed(item[0]);
         } else {
@@ -40,7 +40,7 @@ export default function AutocompleteAccountBank({
                 getOptionLabel={(option: any) =>
                     option.tenChuThe ? option.tenRutGon.concat(` - `, option.soTaiKhoan, ` - `, option.tenChuThe) : ''
                 }
-                renderInput={(params) => <TextField {...params} label="Tìm kiếm" />}
+                renderInput={(params) => <TextField {...params} label="Chọn tài khoản" />}
                 renderOption={(props, option) => {
                     return (
                         <div key={option.id == '' ? 0 : option.id}>
@@ -63,18 +63,27 @@ export default function AutocompleteAccountBank({
                                         item
                                         key={option.id}
                                         sx={{ width: 'calc(100% - 44px)', wordWrap: 'break-word' }}>
-                                        <Typography style={{ fontSize: '14px', fontWeight: 500 }}>
-                                            {option.tenChuThe.toString().concat(` (`, `${option.soTaiKhoan}`, `)`)}
-                                        </Typography>
-                                        <Box
-                                            component="span"
-                                            style={{
-                                                fontWeight: 500,
-                                                color: '#acaca5',
-                                                fontSize: '12px'
-                                            }}>
-                                            {option.tenNganHang.toString().concat(` ${option.maNganHang}`)}
-                                        </Box>
+                                        <Stack direction={'row'} spacing={2}>
+                                            <Stack sx={{ border: '1px solid #ccc', borderRadius: '4px' }}>
+                                                <img src={option?.logoNganHang} style={{ width: 135, height: 50 }} />
+                                            </Stack>
+                                            <Stack alignItems={'center'}>
+                                                <Typography style={{ fontSize: '14px', fontWeight: 500 }}>
+                                                    {option.tenChuThe
+                                                        .toString()
+                                                        .concat(` (`, `${option.soTaiKhoan}`, `)`)}
+                                                </Typography>
+                                                <Box
+                                                    component="span"
+                                                    style={{
+                                                        fontWeight: 500,
+                                                        color: '#acaca5',
+                                                        fontSize: '12px'
+                                                    }}>
+                                                    {option.tenNganHang.toString().concat(` ${option.maNganHang}`)}
+                                                </Box>
+                                            </Stack>
+                                        </Stack>
                                     </Grid>
                                 )}
                             </li>
