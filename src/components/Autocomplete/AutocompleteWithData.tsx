3e4d857a43b98402abbca6040c316e26a27ce9c9
NhuongDT <inftech.ts91@gmail.com>
2024-04-02 14:43:08 +0700

    AutocompleteWithData: add interface IPropsAutocompleteWithData

--- src/components/Autocomplete/AutocompleteWithData.tsx
+++ src/components/Autocomplete/AutocompleteWithData.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Autocomplete, Grid, TextField, Typography, Box, Stack } from '@mui/material';
+import { Autocomplete, Grid, TextField, Typography, Stack } from '@mui/material';
 import { IDataAutocomplete } from '../../services/dto/IDataAutocomplete';
 
-export default function AutocompleteWithData({
-    handleChoseItem,
-    idChosed,
-    lstData,
-    label,
-    helperText,
-    optionLabel = { label1: '', label2: '' }
-}: any) {
+export interface IPropsAutocompleteWithData {
+    label?: string;
+    helperText?: string;
+    idChosed: string | null;
+    lstData: IDataAutocomplete[];
+    optionLabel?: { label1?: string; label2?: string };
+    handleChoseItem: (item: IDataAutocomplete) => void;
+}
+
+export default function AutocompleteWithData(props: IPropsAutocompleteWithData) {
+    const { label, helperText, idChosed, lstData, optionLabel, handleChoseItem } = props;
+
     const [itemChosed, setItemChosed] = useState<IDataAutocomplete | null>(null);
     React.useEffect(() => {
         const item = lstData?.filter((x: IDataAutocomplete) => x.id == idChosed);
-        if (item.length > 0) {
+        if (item?.length > 0) {
             setItemChosed(item[0]);
         } else {
             setItemChosed(null);
@@ -44,31 +48,39 @@ export default function AutocompleteWithData({
                         <li {...props} style={{ borderBottom: '1px dashed var(--border-color)' }}>
                             <Grid container alignItems="center">
                                 <Grid item xs={12}>
-                                    <Stack
-                                        direction={'row'}
-                                        justifyContent={'space-between'}
-                                        paddingLeft={0}
-                                        marginLeft={0}>
-                                        <Stack direction={'row'} spacing={1}>
-                                            <Typography style={{ fontSize: '13px' }} color={'#acaca5'}>
-                                                {optionLabel?.label1}
-                                            </Typography>
-                                            <Typography style={{ fontSize: '13px', fontWeight: 500 }}>
-                                                {option.text1}
-                                            </Typography>
-                                        </Stack>
-                                        {/* chỉ hiển thị nếu text2!='' */}
-                                        {option.text2 && (
-                                            <Stack
-                                                direction={'row'}
-                                                spacing={1}
-                                                style={{
-                                                    fontSize: '13px'
-                                                }}>
-                                                <span style={{ color: '#acaca5' }}>{optionLabel?.label2}</span>
-                                                <span> {option.text2}</span>
+                                    <Stack direction={'row'} spacing={1}>
+                                        <img
+                                            width={40}
+                                            height={40}
+                                            src={option?.imgUrl}
+                                            style={{ borderRadius: '100%' }}
+                                        />
+                                        <Stack
+                                            direction={'row'}
+                                            justifyContent={'space-between'}
+                                            paddingLeft={0}
+                                            marginLeft={0}>
+                                            <Stack direction={'row'} spacing={1}>
+                                                <Typography style={{ fontSize: '13px' }} color={'#acaca5'}>
+                                                    {optionLabel?.label1}
+                                                </Typography>
+                                                <Typography style={{ fontSize: '13px', fontWeight: 500 }}>
+                                                    {option.text1}
+                                                </Typography>
                                             </Stack>
-                                        )}
+                                            {/* chỉ hiển thị nếu text2!='' */}
+                                            {option.text2 && (
+                                                <Stack
+                                                    direction={'row'}
+                                                    spacing={1}
+                                                    style={{
+                                                        fontSize: '13px'
+                                                    }}>
+                                                    <span style={{ color: '#acaca5' }}>{optionLabel?.label2}</span>
+                                                    <span> {option.text2}</span>
+                                                </Stack>
+                                            )}
+                                        </Stack>
                                     </Stack>
                                 </Grid>
                             </Grid>
