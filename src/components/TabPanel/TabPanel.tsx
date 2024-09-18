62b1433e894f89ec3a8549b2d889cbf7bacc9051
NhuongDT <inftech.ts91@gmail.com>
2024-09-18 22:08:55 +0700

    add some style (.grid-table.)

--- src/components/TabPanel/TabPanel.tsx
+++ src/components/TabPanel/TabPanel.tsx
@@ -10,7 +10,7 @@ interface TabPanelProps {
 export default function TabPanel(props: TabPanelProps) {
     const { children, value, index, style, ...other } = props;
     return (
-        <Stack hidden={value !== index} {...other}>
+        <Stack hidden={value !== index} {...other} width="100%">
             {value === index && (
                 <Box marginTop={3} style={style}>
                     {children}
