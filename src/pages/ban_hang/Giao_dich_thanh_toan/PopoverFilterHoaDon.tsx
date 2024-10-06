bcc45d9c84eba1fa777f92c704a4a42e8bf116a2
NhuongDT <inftech.ts91@gmail.com>
2024-10-06 15:26:43 +0700

    PopoverFilterHoaDon: add filter (thoiGian)

--- src/pages/ban_hang/Giao_dich_thanh_toan/PopoverFilterHoaDon.tsx
+++ src/pages/ban_hang/Giao_dich_thanh_toan/PopoverFilterHoaDon.tsx
@@ -1,8 +1,8 @@
-import { Popover, Stack, Typography, Button, Grid } from '@mui/material';
+import { Popover, Stack, Typography, Button, Grid, TextField } from '@mui/material';
 import MultipleAutocompleteWithData from '../../../components/Autocomplete/MultipleAutocompleteWithData';
 import { IList } from '../../../services/dto/IList';
 import ClearIcon from '@mui/icons-material/Clear';
-import { ISelect } from '../../../lib/appconst';
+import { DateType, ISelect, LoaiChungTu } from '../../../lib/appconst';
 import ListRadio from '../../../components/Radio/ListRadio';
 import ListCheckbox from '../../../components/Checkbox/ListCheckbox';
 import { useState, useEffect } from 'react';
@@ -10,10 +10,14 @@ import { HoaDonRequestDto } from '../../../services/dto/ParamSearchDto';
 import { TrangThaiNo, TrangThaiHoaDon } from '../../../services/ban_hang/HoaDonConst';
 import { IPropsPopoverFilter } from '../../../services/dto/IPropsComponent';
 import suggestStore from '../../../stores/suggestStore';
+import DateFilterCustom from '../../../components/DatetimePicker/DateFilterCustom';
+import { addDays, format } from 'date-fns';
 
 export default function PopoverFilterHoaDon(props: IPropsPopoverFilter<HoaDonRequestDto>) {
     const { id, anchorEl, handleClose, handleApply, paramFilter } = props;
     const [paramFilterThis, setParamFilterThis] = useState<HoaDonRequestDto>({} as HoaDonRequestDto);
+    const [anchorDateEl, setAnchorDateEl] = useState<HTMLDivElement | null>(null);
+    const openDateFilter = Boolean(anchorDateEl);
 
     useEffect(() => {
         // set again idChiNhanh if change at header
@@ -21,6 +25,9 @@ export default function PopoverFilterHoaDon(props: IPropsPopoverFilter<HoaDonReq
             ...paramFilterThis,
             idChiNhanhs: paramFilter?.idChiNhanhs,
             trangThaiNos: paramFilter?.trangThaiNos,
+            fromDate: paramFilter?.fromDate,
+            toDate: paramFilter?.toDate,
+            dateType: paramFilter?.dateType ?? DateType.THANG_NAY,
             trangThais: paramFilter?.trangThais ?? [TrangThaiHoaDon.HOAN_THANH]
         });
     }, [paramFilter?.idChiNhanhs]);
@@ -31,6 +38,17 @@ export default function PopoverFilterHoaDon(props: IPropsPopoverFilter<HoaDonReq
             idChiNhanhs: lstChosed?.map((x) => {
                 return x.id;
             })
+        });
+    };
+
+    const onApplyFilterDate = async (from: string, to: string, dateType: string) => {
+        setAnchorDateEl(null);
+        setParamFilterThis({
+            ...paramFilterThis,
+            fromDate: from,
+            toDate: format(addDays(new Date(to), 1), 'yyyy-MM-dd'),
+            currentPage: 1,
+            dateType: dateType
         });
     };
 
@@ -64,6 +82,44 @@ export default function PopoverFilterHoaDon(props: IPropsPopoverFilter<HoaDonReq
                                     })}
                                     handleChosedItem={choseChiNhanh}
                                 />
+                                {(paramFilter?.idLoaiChungTus?.includes(LoaiChungTu.THE_GIA_TRI) ||
+                                    paramFilter?.idLoaiChungTus?.includes(LoaiChungTu.PHIEU_DIEU_CHINH_TGT)) && (
+                                    <Stack>
+                                        <Typography variant="body2" fontWeight={500}>
+                                            Thời gian
+                                        </Typography>
+                                        <TextField
+                                            label=""
+                                            size="small"
+                                            fullWidth
+                                            variant="outlined"
+                                            sx={{
+                                                paddingTop: 1,
+                                                '& .MuiInputBase-root': {
+                                                    height: '40px!important'
+                                                },
+                                                backgroundColor: 'white'
+                                            }}
+                                            onClick={(event) => setAnchorDateEl(event.currentTarget)}
+                                            value={`${format(
+                                                new Date(paramFilterThis?.fromDate ?? new Date()),
+                                                'dd/MM/yyyy'
+                                            )} - ${format(
+                                                new Date(paramFilterThis?.toDate ?? new Date()),
+                                                'dd/MM/yyyy'
+                                            )}`}
+                                        />
+                                        <DateFilterCustom
+                                            id="popover-date-filter"
+                                            open={openDateFilter}
+                                            anchorEl={anchorDateEl}
+                                            onClose={() => setAnchorDateEl(null)}
+                                            onApplyDate={onApplyFilterDate}
+                                            dateTypeDefault={paramFilterThis?.dateType ?? DateType.THANG_NAY}
+                                        />
+                                    </Stack>
+                                )}
+
                                 <Stack>
                                     <Typography variant="body2" fontWeight={500}>
                                         Trạng thái
@@ -90,28 +146,31 @@ export default function PopoverFilterHoaDon(props: IPropsPopoverFilter<HoaDonReq
                                         />
                                     </Stack>
                                 </Stack>
-                                <Stack>
-                                    <Typography variant="body2" fontWeight={500}>
-                                        Trạng thái nợ
-                                    </Typography>
-                                    <Stack paddingTop={1}>
-                                        <ListCheckbox
-                                            arrValueDefault={paramFilterThis.trangThaiNos as unknown as number[]}
-                                            lstOption={
-                                                [
-                                                    { value: TrangThaiNo.CON_NO, text: 'Còn nợ' },
-                                                    { value: TrangThaiNo.HET_NO, text: 'Hết nợ' }
-                                                ] as ISelect[]
-                                            }
-                                            onChange={(arrChosed: number[] | string[]) =>
-                                                setParamFilterThis({
-                                                    ...paramFilterThis,
-                                                    trangThaiNos: arrChosed as number[]
-                                                })
-                                            }
-                                        />
+                                {!paramFilter?.idLoaiChungTus?.includes(LoaiChungTu.PHIEU_DIEU_CHINH_TGT) && (
+                                    <Stack>
+                                        <Typography variant="body2" fontWeight={500}>
+                                            Trạng thái nợ
+                                        </Typography>
+                                        <Stack paddingTop={1}>
+                                            <ListCheckbox
+                                                arrValueDefault={paramFilterThis.trangThaiNos as unknown as number[]}
+                                                lstOption={
+                                                    [
+                                                        { value: TrangThaiNo.CON_NO, text: 'Còn nợ' },
+                                                        { value: TrangThaiNo.HET_NO, text: 'Hết nợ' }
+                                                    ] as ISelect[]
+                                                }
+                                                onChange={(arrChosed: number[] | string[]) =>
+                                                    setParamFilterThis({
+                                                        ...paramFilterThis,
+                                                        trangThaiNos: arrChosed as number[]
+                                                    })
+                                                }
+                                            />
+                                        </Stack>
                                     </Stack>
-                                </Stack>
+                                )}
+
                                 <Stack direction={'row'} justifyContent={'end'} spacing={1}>
                                     <Button variant="outlined" fullWidth onClick={handleClose}>
                                         Đóng
