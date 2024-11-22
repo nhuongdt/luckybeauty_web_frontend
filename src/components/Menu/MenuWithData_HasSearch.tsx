29c2a3fac04862fd2d06e1e6e26c3ea2c997be04
NhuongDT <inftech.ts91@gmail.com>
2024-11-22 08:34:06 +0700

    add checkbox

--- src/components/Menu/MenuWithData_HasSearch.tsx
+++ src/components/Menu/MenuWithData_HasSearch.tsx
@@ -2,6 +2,7 @@ import { Checkbox, Menu, MenuItem, Stack, TextField, Typography } from '@mui/mat
 import SearchIcon from '@mui/icons-material/Search';
 import { IList } from '../../services/dto/IList';
 import { CSSProperties } from 'styled-components';
+import { useState } from 'react';
 
 type IPropsMenu = {
     open: boolean;
@@ -9,6 +10,7 @@ type IPropsMenu = {
     lstData: IList[];
     anchorEl: HTMLElement | null;
     handleChoseItem?: (item: IList) => void;
+    choseMultipleItem?: (arrId: string[]) => void;
     handleClose: () => void;
     style?: CSSProperties;
 };
@@ -18,8 +20,39 @@ export default function MenuWithDataHasSearch({
     lstData,
     anchorEl,
     handleChoseItem,
+    choseMultipleItem,
     handleClose
 }: IPropsMenu) {
+    const [arrIdChosed, setArrIdChosed] = useState<string[]>([]);
+
+    const changeCheckBox = (id: string, isCheck: boolean) => {
+        let arrNew: string[];
+        if (isCheck) {
+            arrNew = [id, ...arrIdChosed];
+        } else {
+            arrNew = arrIdChosed?.filter((x) => x !== id);
+        }
+        setArrIdChosed(arrNew);
+        if (choseMultipleItem) {
+            choseMultipleItem(arrNew);
+        }
+    };
+
+    const changeCheckAll = (isCheck: boolean) => {
+        let arrNew: string[];
+        if (isCheck) {
+            arrNew = lstData?.map((x) => {
+                return x.id;
+            });
+        } else {
+            arrNew = [];
+        }
+        setArrIdChosed(arrNew);
+        if (choseMultipleItem) {
+            choseMultipleItem(arrNew);
+        }
+    };
+
     return (
         <Menu
             id="menu-with-data-has-search"
@@ -30,6 +63,17 @@ export default function MenuWithDataHasSearch({
                 'aria-labelledby': 'basic-button'
             }}>
             <TextField size="small" InputProps={{ startAdornment: <SearchIcon /> }} variant="standard" />
+
+            {hasCheckBox && (
+                <Checkbox
+                    sx={{
+                        '&.MuiCheckbox-root': {
+                            paddingLeft: 0
+                        }
+                    }}
+                    onChange={(e) => changeCheckAll(e.currentTarget.checked)}
+                />
+            )}
             {lstData?.map((x, index) => (
                 <MenuItem
                     key={index}
@@ -44,6 +88,7 @@ export default function MenuWithDataHasSearch({
                                         paddingLeft: 0
                                     }
                                 }}
+                                onChange={(e) => changeCheckBox(x.id, e.currentTarget.checked)}
                             />
                         )}
                         <Typography variant="body1"> {x.text}</Typography>
